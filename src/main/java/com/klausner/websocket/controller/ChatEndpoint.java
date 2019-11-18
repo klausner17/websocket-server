475bc0c4f1709af55911843b380eceeed17f5b5d
Klausner Augusto <klausner17@gmail.com>
2019-11-18 19:31:42 -0300

    initial commit

---
+++ src/main/java/com/klausner/websocket/controller/ChatEndpoint.java
@@ -0,0 +1,54 @@
+package com.klausner.websocket.controller;
+
+import com.klausner.websocket.decoder.MessageDecoder;
+import com.klausner.websocket.encoder.MessageEncoder;
+import com.klausner.websocket.model.Message;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+
+@Slf4j
+@ServerEndpoint(value = "/chat/{username}",
+        decoders = MessageDecoder.class,
+        encoders = MessageEncoder.class)
+public class ChatEndpoint {
+
+    private static HashMap<String, Session> chatEndpoints = new HashMap<>();
+    private static HashMap<String, String> users = new HashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        chatEndpoints.put(username, session);
+        users.put(session.getId(), username);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        chatEndpoints.remove(session.getId());
+    }
+
+    @OnMessage
+    public void onMessage(Session session, Message message) {
+        message.setFrom(users.get(session.getId()));
+        if (message.getTo() != null) {
+            sendMessage(message);
+        }
+    }
+
+    private void sendMessage(Message message) {
+        Session session = chatEndpoints.get(message.getTo());
+        try {
+            session.getBasicRemote().sendObject(message);
+        } catch (IOException | EncodeException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/klausner/websocket/model/Message.java
@@ -0,0 +1,16 @@
+package com.klausner.websocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String from;
+    private String to;
+    private String content;
+}
